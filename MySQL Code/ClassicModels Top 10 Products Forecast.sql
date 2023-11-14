569ed5987cd0abe13cef87d160194a11fbd78830
gmeza1 <126731057+gmeza1@users.noreply.github.com>
2023-11-14 01:40:58 -0800

    Add files via upload

---
+++ MySQL Code/ClassicModels Top 10 Products Forecast.sql
@@ -0,0 +1,34 @@
+-- Obtain all individual orders for the top 10 quantity sold products (Product Name, Order Date, Quantity)
+WITH top_ten_products AS (  -- obtains top 10 products
+	SELECT 
+		od.productCode, 
+		SUM(quantityOrdered) AS quantity_ordered
+	FROM
+		(SELECT 
+			orderNumber
+		FROM
+			orders
+		WHERE
+			orderStatus = 'Resolved'
+				OR orderStatus = 'Shipped') O
+			JOIN
+		orderdetails od ON o.orderNumber = od.orderNumber
+	GROUP BY od.productCode
+	ORDER BY quantity_ordered DESC
+	LIMIT 10
+)
+SELECT DISTINCT
+	p.productName,
+    o.orderDate,
+    od.quantityOrdered
+FROM
+	top_ten_products ttp
+		JOIN
+	orderdetails od ON ttp.productCode = od.productCode
+		JOIN
+	orders o ON od.orderNumber = o.orderNumber
+		JOIN
+	products p ON ttp.productCode = p.productCode
+WHERE
+	o.orderStatus = "Resolved"
+		OR o.orderStatus = "Shipped";
